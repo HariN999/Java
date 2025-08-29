01ce4a6fb05da55fe85706f8eedd5d39a8622f57
Hariharan Narlakanti <143284109+HariN999@users.noreply.github.com>
2025-08-29 15:30:01 +0530

    Add files via upload

---
+++ Strings/ReverseWordsInSentence.java
@@ -0,0 +1,27 @@
+package Day10;
+
+import java.util.*;
+public class ReverseWordsInSentence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string : ");
+        String a = sc.nextLine();
+        String res = "";
+        String word = "";
+
+        for (int i = 0; i < a.length(); i++) {
+            char ch = a.charAt(i);
+            if (ch != ' ') {
+                word = ch + word;
+            } else {
+                res = res + word + " "; 
+                word = "";
+            }
+        }
+        res = res + word; 
+
+        System.out.println("Original String : " + a);
+        System.out.println("Reversed String : " + res.trim());
+        sc.close();
+    }
+}
